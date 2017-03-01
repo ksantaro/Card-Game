6e9c634f2be17488838346d273c9a17f5adf1389
ksantaro <ubkenneths@gmail.com>
2017-02-28 19:56:06 -0800

    Unity addition

---
+++ Assets/Scripts/Draggable.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+	public Vector2 dragOffset = new Vector2(0f, 0f);
+	public Transform parentToReturnTo = null;
+
+	public void OnBeginDrag(PointerEventData eventData) {
+		//Debug.Log("OnBeginDrag");
+
+		parentToReturnTo = this.transform.parent; //Set Parent
+		this.transform.SetParent (this.transform.parent.parent); //Transform up by one parent
+		GetComponent<CanvasGroup> ().blocksRaycasts = false; // set casts to false
+	
+		dragOffset = eventData.position - (Vector2)this.transform.position; //Set Offset position
+
+	}
+	public void OnDrag(PointerEventData eventData) {
+		//Debug.Log("OnDraggggg");
+
+		this.transform.position = eventData.position - dragOffset; //drag based on diffrence of offset and position
+	}
+	public void OnEndDrag(PointerEventData eventData) {
+		//Debug.Log("OnEndDrag");
+
+		this.transform.SetParent(parentToReturnTo); // return to orginal parent
+		GetComponent<CanvasGroup> ().blocksRaycasts = true; // set casts to true
+
+	}
+}
---
+++ Assets/Scripts/DropZone.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler {
+
+	public void OnPointerEnter(PointerEventData eventData) {
+		//Debug.Log ("OnPointerEnter");
+	}
+
+	public void OnDrop(PointerEventData eventData) {
+		Debug.Log (eventData.pointerDrag.name + " was dropped to " + gameObject.name);
+
+		Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+		if (d != null) {
+			d.parentToReturnTo = this.transform; // set draggable objects parent to this instance
+		}
+	}
+
+	public void OnPointerExit(PointerEventData eventData) {
+		//Debug.Log ("OnPointerExit");
+	}
+}
